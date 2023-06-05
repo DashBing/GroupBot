b530af3f1cd0737a494efb92e94e6dd870254c08
liqsliu <liqsliu@gmail.com>
2023-06-05 10:13:11 +0000

    20230605_101311: liqsliu commit from GNU/Linux/Linux on x86_64

--- icp.sh
+++ icp.sh
@@ -5,7 +5,8 @@ SH_PATH=${SH_PATH:-$(cd $(dirname ${BASH_SOURCE[0]}) || exit; pwd -P )}
 
 
 _(){
-  curl -m 64 -v "https://v.api.aa1.cn/api/api-lj-gf/index.php?url=$(bash "$SH_PATH/urlencode.sh" "$*")" || exit $?
+  # curl -m 64 -v "https://v.api.aa1.cn/api/api-lj-gf/index.php?url=$(bash "$SH_PATH/urlencode.sh" "$*")" || exit $?
+  curl -m 64 -s "https://v.api.aa1.cn/api/api-lj-gf/index.php?url=$(bash "$SH_PATH/urlencode.sh" "$*")" || exit $?
   echo
 }
 
