9bad92affe16a4bda88937ae36017c715f79342a
liqsliu <liqsliu@gmail.com>
2023-06-07 17:02:22 +0800

    20230607_170222:  commit from Android/Linux on aarch64

---
+++ qy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+SH_PATH=${SH_PATH:-$(cd $(dirname ${BASH_SOURCE[0]}) || exit; pwd -P )}
+
+
+
+
+
+_(){
+  # curl -m 19 -s "http://api.qingyunke.com/api.php?key=free&appid=0&msg=$(bash "$SH_PATH/urlencode.sh" "$*")" || exit $?
+  local res=$(curl -m 19 -s "http://api.qingyunke.com/api.php?key=free&appid=0&msg=$(bash "$SH_PATH/urlencode.sh" "$*")") && {
+
+    local result=$(echo "$res"|jq -r ".result")
+    if [[ "$result" = 0 ]]; then
+      local content=$(echo "$res"|jq -r ".content")
+      echo "$content"
+    else
+      echo "$res"
+    fi
+
+    return
+    true
+  } || exit $?
+  echo
+}
+
+
+if [[ -z "$1" ]]; then
+  echo ".$0 \$str"
+  echo "/$0 \$str"
+  echo '---'
+  echo 'qingyunke.com'
+# elif [[ "$2" == "on" ]]; then
+#   :
+else
+# ai "$@"
+_ "$*"
+fi
+
