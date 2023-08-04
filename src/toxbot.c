e8612e6c7bfecb4deb5b66f329b9d49ea45a993d
liqsliu <liqsliu@gmail.com>
2023-08-04 18:46:03 +0800

    20230804_184603: commit by liqsliu/WSL/GNU/Linux/Linux/x86_64

--- src/toxbot.c
+++ src/toxbot.c
@@ -397,10 +397,10 @@ static void init_public_group(Tox *tox)
     uint8_t chat_id[TOX_GROUP_CHAT_ID_SIZE]="\0";
     if (tox_group_get_chat_id(tox, 0, chat_id, NULL))
     {
-      log_timestamp("first group id is %s", chat_id);
+      log_timestamp("first group id is %x", chat_id);
       uint8_t group_name[TOX_MAX_NAME_LENGTH] = "\0";
       if (tox_group_get_name(tox, 0, group_name, NULL))
-        log_timestamp("first group name is %s", chat_id);
+        log_timestamp("first group name is %s", group_name);
     } else {
       log_timestamp("no joined group");
     }
