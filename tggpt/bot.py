27ccb7970b491911b0452546144fdccfd521946e
liqsliu <liqsliu@gmail.com>
2023-08-11 23:03:57 +0800

    20230811_230357: commit by liqsliu/wtfipfs/GNU/Linux/Linux/x86_64

--- tggpt/bot.py
+++ tggpt/bot.py
@@ -872,7 +872,7 @@ async def mt2tg(msg):
             text=text[5:]
             if not text:
               #  await mt_send(".gpt $text", gateway=msgd["gateway"])
-              await mt_send(".gpt $text\n--\n默认开启了上下文，重置上下文命令“.gpt reset“，如果前面的任务无法结束导致后面的任务卡住也可以使用此命令清理历史任务，多次发送相同的任务请求也能触发bot自动清理历史任务。\n所有数据来自telegram机器人: @littleb_gptBOT ，使用userbot与其对接，因此所有人共享一个上下文，这个问题暂时没办法解决。", gateway=msgd["gateway"])
+              await mt_send(".gpt $text\n--\n默认开启了上下文，重置上下文命令“.gpt reset“，如果前面的任务无法结束导致后面的任务卡住也可以使用此命令清理历史任务，多次发送相同的任务请求也能触发bot自动清理历史任务。\n所有数据来自telegram机器人: https://t.me/littleb_gptBOT ，使用userbot与其对接，因此所有人共享一个上下文，这个问题暂时没办法解决。", gateway=msgd["gateway"])
               return
           elif text == ".se" or text.startswith(".se "):
             #  need_clean = True
@@ -885,7 +885,7 @@ async def mt2tg(msg):
             #  need_clean = True
             text=text[5:]
             if not text:
-              await mt_send(".img $text", gateway=msgd["gateway"])
+              await mt_send(".img $text\n--\nhttps://t.me/littleb_gptBOT", gateway=msgd["gateway"])
               return
             text="/image "+text
           elif text.startswith(".gtz"):
