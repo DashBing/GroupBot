f7b1d4d91dfa7f9884982ee65f89586325c11fd6
liqsliu <liqsliu@gmail.com>
2023-08-17 10:16:54 +0800

    20230817_101654: commit by u0_a277/localhost/Android/Linux/aarch64

--- gm.sh
+++ gm.sh
@@ -18,9 +18,12 @@ fi
 res=$(curl -m 2 -s http://127.0.0.1:4241/api/messages) || exit 0
 if [[ "$res" != "[]" ]]; then
 res=$(echo "$res" | jq 'del(.[].Extra.file[0].Data)') &>/dev/null || exit 0
+res=$(bash "$SH_PATH/msg_for_tox.sh" "$res" 2>&1)
+echo -n "$res"
 export SH_PATH=${SH_PATH:-$(cd $(dirname ${BASH_SOURCE[0]}) || exit; pwd )}
 [[ -e "$SH_PATH/DEBUG" ]] && export LOG_FILE="$HOME/tera/mt.log" || export LOG_FILE=/dev/null
+# bash "$SH_PATH/msg_for_tox.sh" "$res" &>> $LOG_FILE
 echo bash "$SH_PATH/msg_for_tox.sh" "$res" &>> $LOG_FILE
-bash "$SH_PATH/msg_for_tox.sh" "$res" &>> $LOG_FILE
+echo "$res" >> $LOG_FILE
 fi
 
