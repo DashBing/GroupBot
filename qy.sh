d38afba0d0f0b06c29336ea1d43336f2e4f3a3e0
liqsliu <liqsliu@gmail.com>
2023-10-13 14:56:35 +0800

    20231013_145635: commit by liqsliu/WSL/GNU/Linux/Linux/x86_64

--- qy.sh
+++ qy.sh
@@ -8,8 +8,6 @@ SH_PATH=${SH_PATH:-$(cd $(dirname ${BASH_SOURCE[0]}) || exit; pwd -P )}
 
 
 _(){
-  export http_proxy=http://127.0.0.1:6080
-  export https_proxy=http://127.0.0.1:6080
   # curl -m 19 -s "http://api.qingyunke.com/api.php?key=free&appid=0&msg=$(bash "$SH_PATH/urlencode.sh" "$*")" || exit $?
   local res=$(curl -m 19 -s "http://api.qingyunke.com/api.php?key=free&appid=0&msg=$(bash "$SH_PATH/urlencode.sh" "$*")") && {
 
