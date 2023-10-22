6af815e36e07993b7dc527928a170a002110e93a
liqsliu <liqsliu@gmail.com>
2023-10-22 22:14:51 +0800

    20231022_221451: commit by /localhost/Android/Linux/aarch64

--- mt_tengo.sh
+++ mt_tengo.sh
@@ -388,12 +388,10 @@ telegram.*)
     # QT=$( python3 "$SH_PATH/get_msg.py" reply_msg "$(echo "$TEXT" | sed '/^> reply_from_telegram$/,$d')" "$2" "$5" ) || QT=""
     # QT=""
     # [[ -z "$QT" ]] && QT=$( echo "$TEXT" | sed '0,/^_reply_$/d' )
-    QT=$( echo "$TEXT" | sed '0,/^_reply_$/d' )
-    QT=$(echo "$QT" | sed '1s/^G Matrix Telegram Bridge: //' | sed '1s/^G bot: //' | sed 's/^/> /' )
+    QT=$( echo "$TEXT" | sed -e '0,/^_reply_$/d' -e '1s/^G Matrix Telegram Bridge: //' -e '1s/^G bot: //' -e '1s/^G Group: /G admin:/' -e 's/^/> /' )
 
   fi
 
-    QT=$(echo "$QT" | sed '1s/^G Group: /G admin:/')
   # if [[ "$NAME" == "Group" ]]; then
   if [[ "$NAME" == "wtfipfs_fucktelegram" ]]; then
     NAME="admin"
