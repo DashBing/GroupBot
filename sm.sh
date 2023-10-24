1522481c5b49f8c4e281032af527aaff53a9da7b
liqsliu <liqsliu@gmail.com>
2023-10-24 13:51:49 +0800

    20231024_135149: commit by /localhost/Android/Linux/aarch64

--- sm.sh
+++ sm.sh
@@ -37,7 +37,7 @@ curl -m 2 -s -XPOST -H 'Content-Type: application/json' -d "$(bash "$SH_PATH/"ge
 send(){
   local MAX_BYTES=1371
   # local MAX_BYTES=1024
-  MAX_BYTES=$[MAX_BYTES-5-${#username}]
+  MAX_BYTES=$[MAX_BYTES-9-${#username}]
   local text=$1
   if [[ ${#text} -le $MAX_BYTES ]]; then
     echo "sm.sh: the length of msg is ok: ${#text}" &>> $LOG_FILE
@@ -54,6 +54,7 @@ send(){
   while true
   do
     if [[ $((i*MAX_BYTES)) -ge ${#text} ]]; then
+      return 0
       break
     fi
     tmp=${text:$((i*MAX_BYTES)):$MAX_BYTES}
@@ -71,15 +72,15 @@ _push_err(){
   if [[ "$(echo "$res" | jq ".message")" != "null" ]]; then
     date &>> $LOG_FILE
     echo "res :|$res|" >> $LOG_FILE
-    msg=$(echo "$res" | jq ".message")
-    res=$(send "E: mt api: $msg res: $res") 
+    msg=$(echo "$res" | jq ".message") && send "E: mt api res msg: $msg" && return $?
+    # res=$(send "E: mt api: $res") && return $?
     if [[ "$(send "E: mt api: $msg res: $res" | jq ".message")" != "null" ]]; then
       if [[ "$(send "E: mt api: $msg res_b64: $(echo "$res"|base64)" | jq ".message")" != "null" ]]; then
         if [[ "$(send "E: can't send res to mt api: $msg" | jq ".message")" != "null" ]]; then
           if [[ "$(send "E: can't send res to mt api, msg_b64: $(echo "$msg"|base64)" | jq ".message")" != "null" ]]; then
             echo "E: can't send text and err msg to mt api: |$msg|$res|" >> $LOG_FILE
             if [[ "$(send "E: can't send res and err msg to mt api" | jq ".message")" != "null" ]]; then
-              :
+              exit 1
             fi
           fi
         fi
@@ -94,12 +95,13 @@ push_err(){
 
   if ! echo "$res" | jq ".message" &>/dev/null; then
     res=$(send "$res ") || {
-      _push_err "failed to send: $res"
-      exit 1
+      # send "$res"
+      _push_err "wtf: $res"
     }
-    _push_err "$res"
+    # _push_err "$res"
   elif [[ "$(echo "$res" | jq ".message")" != "null" ]]; then
     _push_err "$res"
+    # send "E: $(echo "$res" | jq ".message")"
   else
     if [[ -z "$(echo "$res" | jq -r ".text")" ]]; then
       send "E: empty message"
@@ -119,7 +121,7 @@ res=$(send "$text" 2>"$SH_PATH/error") || {
   r=$(cat "$SH_PATH/error") && rm "$SH_PATH/error"
   res=$(cat "$SH_PATH/out") && rm "$SH_PATH/out"
 }
-  push_err "E: failed to send text: $text|$e|$r"
+  push_err "E: failed to send text: ${text:0:10}...|$e|$r"
   if [[ -n "$res" ]]; then
     push_err "$res"
   fi
