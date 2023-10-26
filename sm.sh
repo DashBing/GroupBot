1d6e1359bfeee36d144ed2c10f3075c4c864e174
liqsliu <liqsliu@gmail.com>
2023-10-26 19:59:46 +0800

    20231026_195946: commit by /localhost/Android/Linux/aarch64

--- sm.sh
+++ sm.sh
@@ -36,16 +36,15 @@ gateway=${4-gateway1}
 # echo "$*" > /tmp/test_sm.sh.txt
 
 wtf(){
-  local text=$(echo "$1" | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' -e 's/\r//g' -e 's/\t/\\t/g' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
+  local text=$1
+text="$(echo "$text" | cut -d '\' --output-delimiter='\\' -f 1- )"
+  text=$(echo "$text" | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' -e 's/\r//g' -e 's/\t/\\t/g' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
 [[ $( echo "$text" | wc -l ) -gt 1 ]] && text=$(echo "$text" | awk '{printf "%s\\n", $0}' | sed "s/\\\\n$//g")
 echo "$text"
 }
 
 gene_res(){
 local text=$1
-text="$(echo "$text" | cut -d '\' --output-delimiter='\\' -f 1- )"
-
-SH_PATH=${SH_PATH:-$(cd $(dirname ${BASH_SOURCE[0]}); pwd )}
 local gateway=${2-gateway1}
 local username=${3-C bot: }
 
