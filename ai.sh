e400aea7b30037ea9dd4cd77109d5a84763515a1
liqsliu <liqsliu@gmail.com>
2023-10-29 08:57:19 +0800

    20231029_085719: commit by u0_a205/localhost/Android/Linux/aarch64

--- ai.sh
+++ ai.sh
@@ -6,6 +6,7 @@ echo
 
 if [[ -z "$1" ]]; then
   echo "google bard"
+  echo "不支持上下文，每次提问都是新的独立的上下文。若想让bot记住上下文，请用bd命令。"
   echo ".ai \$str"
   echo "--"
   echo "link: https://github.com/EvanZhouDev/bard-ai"
