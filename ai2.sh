1ae3e4eb30ca4481ff24fd7f2dbdf785787af087
liqsliu <liqsliu@gmail.com>
2024-01-10 22:24:07 +0800

    20240110_222407: commit by u0_a128/localhost/Android/Linux/aarch64

---
+++ ai2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ai(){
+curl -m 300 -s -XPOST -d "$*" 127.0.0.1:6001 || exit $?
+echo
+}
+
+if [[ -z "$1" ]]; then
+  echo "google bard"
+  echo "不支持上下文，每次提问都是新的独立的上下文。若想让bot记住上下文，请用bd命令。"
+  echo ".ai \$str"
+  echo "--"
+  echo "link: https://github.com/EvanZhouDev/bard-ai"
+  echo "link: https://bard.google.com/"
+# elif [[ "$2" == "on" ]]; then
+#   :
+else
+# ai "$@"
+# ai "$*"
+text=$(bash "$SH_PATH/trans.sh" -brief :en "$*")
+ai "$text
+Please answer in Chinese."
+fi
