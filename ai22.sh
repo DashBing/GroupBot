e025c0b0c0ed7f6c39eee3254748f47c5f235709
liqsliu <liqsliu@gmail.com>
2024-01-10 22:31:21 +0800

    20240110_223121: commit by u0_a128/localhost/Android/Linux/aarch64

--- ai22.sh
+++ ai22.sh
@@ -5,14 +5,7 @@ echo
 }
 
 if [[ -z "$1" ]]; then
-  echo "google bard"
-  echo "不支持上下文，每次提问都是新的独立的上下文。若想让bot记住上下文，请用bd命令。"
-  echo ".ai \$str"
-  echo "--"
-  echo "link: https://github.com/EvanZhouDev/bard-ai"
-  echo "link: https://bard.google.com/"
-# elif [[ "$2" == "on" ]]; then
-#   :
+  echo "把问题翻译成英文，然后交给bard，再把回答翻译为中文。"
 else
 # ai "$@"
 # ai "$*"
