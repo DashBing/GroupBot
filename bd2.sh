5e7896c816883a05251a352d1df4bc8951e264db
liqsliu <liqsliu@gmail.com>
2024-02-21 13:35:40 +0800

    20240221_133540: commit by u0_a128/localhost/Android/Linux/aarch64

--- bd2.sh
+++ bd2.sh
@@ -8,8 +8,6 @@ if [[ -z "$1" ]]; then
   echo "把问题翻译成英文，然后交给bard，同时用英文要求bard用中文回答"
   echo "用法: .bd2 \$str"
 else
-# ai "$@"
-# ai "$*"
 text=$(bash "$SH_PATH/trans.sh" -brief :en "$*")
 ai "$text
 Please answer in Chinese."
--- bd22.sh
+++ bd22.sh
@@ -8,8 +8,6 @@ if [[ -z "$1" ]]; then
   echo "把问题翻译成英文，然后交给bard，再把回答翻译为中文。"
   echo "用法: .bd22 \$str"
 else
-# ai "$@"
-# ai "$*"
 text=$(bash "$SH_PATH/trans.sh" -brief :en "$*")
 bash "$SH_PATH/trans.sh" -brief :zh "$(
 ai "$text"
