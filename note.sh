f928fc170aaaf6dde3297d585eaa2a6c391ace9c
liqsliu <liqsliu@gmail.com>
2024-02-21 21:01:52 +0800

    20240221_210152: commit by u0_a128/localhost/Android/Linux/aarch64

--- note.sh
+++ note.sh
@@ -246,7 +246,7 @@ list)
   echo "$tag"
   # my_decode "$(grep -G "^$tag " "$NOTE_FILE" | cut -d" " -f2- | sed 's/^/\n/g')"
   # my_decode "$(grep -n -F "$tag " "$NOTE_FILE" | cut -d" " -f2- | sed 's/^/\n/g')"
-  my_decode "$(grep -n -F "$tag " "$NOTE_FILE" | sed -r 's|:#[^ ]+ |%|1' | sed 's/^/\n/')"
+  my_decode "$(grep -n -F "$tag " "$NOTE_FILE" | sed -r -e 's|:#[^ ]+ |%|1' -e 's|^|\n|')"
   ;;
 add)
   add "$tag $username$text"
