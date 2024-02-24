0ddbdaa3c31ccb5a21ad5d3b4b0df6fcabd3f444
liqsliu <liqsliu@gmail.com>
2024-02-24 17:21:46 +0800

    20240224_172146: commit by liqsliu/WSL/GNU/Linux/Linux/x86_64

--- sm4gpt.sh
+++ sm4gpt.sh
@@ -2,6 +2,6 @@
 SH_PATH=${SH_PATH:-$(cd $(dirname ${BASH_SOURCE[0]}) || exit; pwd )}
 
 fn=${1-gpt_res}
-text=$(cat "SH_PATH/$fn") && rm "SH_PATH/$fn"
+text=$(cat "$SH_PATH/$fn") && rm "$SH_PATH/$fn"
 gateway=${2-gateway1}
 nohup bash "$SH_PATH/sm2.sh" "C bot" "$text" "4241" "$gateway" &> /dev/null &
