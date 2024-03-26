496f68943216c769e8bfb8a286f980b025f57ba6
liqsliu <liqsliu@gmail.com>
2024-03-26 19:30:00 +0800

    20240326_193000: commit by u0_a128/localhost/Android/Linux/aarch64

--- bcmd.sh
+++ bcmd.sh
@@ -477,6 +477,7 @@ cmds() {
     fi
     ;;
   ban|ban2|ban3|ub|ub2|jid|users|msgs|muc|debugxmpp|sbmode|retr|blocked|b|xmpp)
+    :
     # if [[ "$username" != "X liqsliu: " ]]; then
     #   echo "你输错了命令: $1"
     # fi
