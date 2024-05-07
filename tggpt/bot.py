ebe264da2fbdde752a6611c58050d4250e58e6bd
liqsliu <liqsliu@gmail.com>
2024-05-07 11:30:10 +0800

    20240507_113010: commit by liqsliu/WSL/GNU/Linux/Linux/x86_64

--- tggpt/bot.py
+++ tggpt/bot.py
@@ -11,28 +11,6 @@ from telethon.tl.types import KeyboardButton, KeyboardButtonUrl
 #  HOME = os.environ.get("HOME")
 
 import logging
-logger = logging.getLogger(__name__)
-class NoParsingFilter(logging.Filter):
-  def filter(self, record):
-    #  if record.name == 'tornado.access' and record.levelno == 20:
-    if record.name == 'httpx':
-      if record.message == 'HTTP Request: GET https://qwen-qwen1-5-72b-chat.hf.space/--replicas/3kh1x/heartbeat/f6f9ef32-4cc6-470e-9bfb-957b4bc6ff5d "HTTP/1.1 404 Not Found"':
-        return False
-      else:
-        logger.info(f"文本不对: {record.message}")
-    else:
-      if record.levelno == 20:
-        pprint(record)
-        return False
-        if record.message == 'HTTP Request: GET https://qwen-qwen1-5-72b-chat.hf.space/--replicas/3kh1x/heartbeat/f6f9ef32-4cc6-470e-9bfb-957b4bc6ff5d "HTTP/1.1 404 Not Found"':
-          logger.info(f"找到了文本，name不对: {record}")
-          return False
-        if '404 Not Found' in record.message:
-           logger.info(f"根据关键词找到了文本，name不对: {record}")
-           return False
-    return True
-
-logger.addFilter(NoParsingFilter())
 
 
 import asyncio
