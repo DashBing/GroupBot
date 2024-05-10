80facb2bfdde6c6fbc05f269eae6fb96a5493ff5
liqsliu <liqsliu@gmail.com>
2024-05-10 21:00:26 +0800

    20240510_210026: commit by liqsliu/WSL/GNU/Linux/Linux/x86_64

--- tggpt/bot.py
+++ tggpt/bot.py
@@ -3044,7 +3044,8 @@ async def parse_xmpp_msg(msg):
       else:
         await send("not allowed", msg.from_)
     elif msg.type_ == PresenceType.AVAILABLE:
-      pprint(msg.xep0045_muc_user)
+      for i in msg.xep0045_muc_user.items:
+        pprint(i)
       print(f"上线: {msg.from_} {msg.status}")
     elif msg.type_ == PresenceType.UNAVAILABLE:
       print(f"离线: {msg.from_} {msg.status}")
