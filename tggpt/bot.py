6007335793b962a1c9a07f420463c5ee8f7c78f0
liqsliu <liqsliu@gmail.com>
2024-05-10 08:28:51 +0800

    20240510_082851: commit by liqsliu/WSL/GNU/Linux/Linux/x86_64

--- tggpt/bot.py
+++ tggpt/bot.py
@@ -2999,6 +2999,9 @@ async def parse_xmpp_msg(msg):
   if text is None:
     print("跳过空消息: %s %s %s %s" % (msg.type_, msg.from_, msg.to, msg.body))
     return
+  if str(msg.to_.bare()) == myjid:
+    print("跳过自己发送的消息%s %s %s %s %s" % (msg.type_, msg.id_,  str(msg.from_), msg.to, msg.body))
+    return
   pprint(msg)
   print("%s %s %s %s %s" % (msg.type_, msg.id_,  str(msg.from_), msg.to, msg.body))
   if text == "ping":
