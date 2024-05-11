76952fdb5fdbea93f2ac8d1ad2f2a7d1032dab74
liqsliu <liqsliu@gmail.com>
2024-05-11 10:57:57 +0800

    20240511_105757: commit by liqsliu/WSL/GNU/Linux/Linux/x86_64

--- tggpt/bot.py
+++ tggpt/bot.py
@@ -3127,7 +3127,7 @@ async def parse_xmpp_msg(msg):
               return
             j = jids[jid]
             if j[0] != msg.from_.resource:
-              await send(f"改名通知: {hide_nick(j[0]} -> {hide_nick(msg)}", muc, fromname=".ban {muc}/{msg.from_.resource}")
+              await send(f"改名通知: {hide_nick(j[0])} -> {hide_nick(msg)}", muc, fromname=".ban {muc}/{msg.from_.resource}")
               j[0] = msg.from_.resource
           else:
             welcome=f"欢迎 {hide_nick(msg)} ,如需查看群介绍，请发送 “.help”。该消息来自机器人(bot)，可不予理会。"
