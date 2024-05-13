eb37bca518d951b7bbffaafb06826251df005aed
liqsliu <liqsliu@gmail.com>
2024-05-13 18:15:54 +0800

    20240513_181554: commit by liqsliu/WSL/GNU/Linux/Linux/x86_64

--- tggpt/bot.py
+++ tggpt/bot.py
@@ -2792,6 +2792,7 @@ async def parse_tg_msg(event):
     #  print("W: skip unknown chat_id: %s %s" % (event.chat_id, msg.text[:64]))
     res, nick, delay = await print_tg_msg(event)
     if event.chat_id in bridges:
+      logger.info(f"转发桥接消息: {event.chat_id} -> {bridges[event.chat_id]}: {res}")
       await send(msg.text, jid=bridges[event.chat_id], name=nick, nick=nick, delay=delay)
     else:
       await send(res, name="", nick=nick, delay=delay)
