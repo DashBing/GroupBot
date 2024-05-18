f97bb69eb5cc4e893dbb5f9c41008938a1a595b0
liqsliu <liqsliu@gmail.com>
2024-05-18 12:03:29 +0800

    20240518_120329: commit by liqsliu/WSL/GNU/Linux/Linux/x86_64

--- tggpt/bot.py
+++ tggpt/bot.py
@@ -3462,17 +3462,18 @@ async def parse_xmpp_msg(msg):
       #    pprint(i)
     elif msg.type_ == PresenceType.UNAVAILABLE:
       print(f"离线: {msg.from_} {msg.status}")
-      if muc in my_groups:
-        pass
-      else:
-        await sendg(f"离线: {msg.from_} {msg.status}")
+      #  if muc in my_groups:
+      #    pass
+      #  else:
+      #    await sendg(f"离线: {msg.from_} {msg.status}")
       if hasattr(msg, "xep0045_muc_user"):
-        pass
-        #  if msg.xep0045_muc_user:
-        #  pprint(msg.xep0045_muc_user)
+        if msg.xep0045_muc_user is None:
+          #  if msg.xep0045_muc_user:
+          #  pprint(msg.xep0045_muc_user)
+          await sendg(f"离线: {msg.from_} {msg.status}")
       else:
-        pprint(msg)
-        await sendg(f"离线: {msg.from_} {msg.status}")
+        #  pprint(msg)
+        await sendg(f"离线n: {msg.from_} {msg.status}")
     else:
       pprint(msg)
     return
